080dd9af6a65c1a80e8a7d1a4ef4234e7e67c283
JonathanKimelfeld <jonathan.kimelfeld@mail.huji.ac.il>
2022-06-01 21:54:23 +0300

    Initial Commit, let's rock it!

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+path = 'C:\\Users\\User\\PycharmProjects\\Hackathon-IML\\train.feats.csv'
+df = pd.read_csv(path, encoding='utf8')
+
+
+if __name__ == '__main__':
+    print(df.head())
